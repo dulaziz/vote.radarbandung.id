71f74dbc5a0fd1f6879b2d94e67df4bda28652b7
Abdul Aziz <dulaziz>
2023-09-12 17:59:49 +0700

    social

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -13,7 +13,7 @@
         </div>
 
         <div class="col-md-3">
-          <h5 class="text-uppercase mb-4 fw-bold">Soccial</h5>
+          <h5 class="text-uppercase mb-4 fw-bold">Social</h5>
           <hr>
           <p>
             <a class="text-decoration-none text-light" href="https://www.youtube.com/@radarbandungnews" target="_blank">
